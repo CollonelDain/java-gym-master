e0a7b4a7b5eb1325f33392bc84f7d240847ce245
CollonelDain <d4nillpopoff@yandex.ru>
2026-02-04 14:20:40 +0300

    Реализован класс Timetable для работы с расписанием тренировок

--- src/ru/yandex/practicum/gym/Timetable.java
+++ src/ru/yandex/practicum/gym/Timetable.java
@@ -4,17 +4,27 @@ import java.util.*;
 
 public class Timetable {
 
-    private /* как это хранить??? */ timetable;
+    private Map<DayOfWeek, SortedMap<TimeOfDay, List<TrainingSession>>> timetable = new HashMap<>();
 
     public void addNewTrainingSession(TrainingSession trainingSession) {
-        //сохраняем занятие в расписании
+        SortedMap<TimeOfDay, List<TrainingSession>> dayMap = timetable.computeIfAbsent(
+                trainingSession.getDayOfWeek(),
+                k -> new TreeMap<>()
+        );
+
+        dayMap.computeIfAbsent(
+                trainingSession.getTimeOfDay(),
+                k -> new ArrayList<>()
+        ).add(trainingSession);
     }
 
-    public /* непонятно, что возвращать */ getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
-        //как реализовать, тоже непонятно, но сложность должна быть О(1)
+
+    //TODO: возможно, не было смысла возвращать emptyCollection и стоит вернуться к returnForDefault new Collection<>()
+    public SortedMap<TimeOfDay, List<TrainingSession>> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
+        return timetable.getOrDefault(dayOfWeek, Collections.emptySortedMap());
     }
 
-    public /* непонятно, что возвращать */ getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
-        //как реализовать, тоже непонятно, но сложность должна быть О(1)
+    public List<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
+        return getTrainingSessionsForDay(dayOfWeek).getOrDefault(timeOfDay, Collections.emptyList());
     }
 }
